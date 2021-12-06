a82ae8d5ae8976525102b4d11aed6b31e5b21fb6
Victor C. Cole <73194198+vitorzera@users.noreply.github.com>
2021-12-06 13:11:22 -0300

    Add files via upload

---
+++ Code's/Controle/For_1.java
@@ -0,0 +1,18 @@
+public class For_1 {
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 0; contador++) {
+            System.out.printf("i = %d\n", contador);
+        }
+    }
+}
+
+//        int x = 2; 
+//        for(;x < 10;) {
+//            System.out.printf("x = " + x);
+//            x++;
+//        }
+
+//        for(;true;) {
+//            System.out.println("*-*");
+//        }
+//    }
---
+++ Code's/Controle/While_Determinado.java
@@ -0,0 +1,11 @@
+public class While_Determinado {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 10) {
+            System.out.printf("i = %d\n", contador);
+            contador++;
+        }
+    }
+    
+}
